dbe963f229d03957466eb91cee054166eca0b4bc
Thoma77s <thomassiffc3@gmail.com>
2022-01-07 20:38:51 -0500

    finish javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,36 @@
-const store = window.localStorage;
+document.addEventListener("DOMContentLoaded", function() {
+  var timeNow = moment().format("ha");
+  var dt = new Date();
+  document.getElementById("currentDay").innerHTML = dt.toLocaleDateString();
+  var hasPassed = false;
+  var saveBtn = document.querySelectorAll(".saveBtn");
+  var textAreaEls = document.querySelectorAll(".edit_cont")
+  
+  textAreaEls.forEach(element => {
+    if (element.id === timeNow) {
+      hasPassed = true;
+      element.classList.add("present");
+    } else if (hasPassed) {
+      element.classList.add("future");
+    } else {
+      element.classList.add("past");
+    };
 
-const container = $(".container");
-
-const now = moment();
-
-const currentTime = { text: moment().format("h:00 A"), hour: moment().hour() };
-
-$("#day").text(now.format("dddd MMMM DD, YYYY"));
-
-const range = (start, end, step) => {
-  return Array.from(
-    Array.from(Array(Math.ceil((end - start) / step)).keys()),
-    (x) => start + x * step
-  );
-};
-
-const hoursOfTheDay = Array.from(new Array(24)).map((v, i) => {
-  const text = moment().hour(i).format("h:00 A");
-  const hour = moment().hour(i);
-  return { text, hour };
-});
-
-function color(time) {
-  return time.text === currentTime.text
-    ? "bg-red-300"
-    : time.hour < now
-    ? "bg-gray-300"
-    : "bg-green-200";
-}
-
-hoursOfTheDay.forEach((hr) => {
-  const grid = $(
-    `<form data-name="${hr.text}" class="grid grid-cols-12  border-gray-500 "></form>.`
-  );
-
-  const time = $(
-    `<div class="flex items-center justify-center col-span-2 h-16">${hr.text}</div>`
-  );
-
-  const textArea = $(
-    `<textarea name="${
-      hr.text
-    }" maxLength="50" style="resize: none; overflow: hidden;" class="col-span-8 h-16 p-6 ${color(
-      hr
-    )}">${store.getItem(hr.text) || ""}</textarea>`
-  );
-
-  textArea.keydown((e) => {
-    if (e.keyCode == 13 && !e.shiftKey) {
-      e.preventDefault();
-      return false;
+    if (localStorage.getItem(element.id) !== null) {
+      element.value = localStorage.getItem(element.id)
     }
   });
 
-  const saveButton = $(
-    `<button type="submit" class="col-span-2 h-16 bg-indigo-500 text-white font-bold hover:bg-indigo-400 transition duration-500 ease-in-out"><i class="fas fa-save text-xl"></i></button>`
-  );
-
-  grid.submit((e) => {
-    e.preventDefault();
-
-    const value = $(`textarea[name="${hr.text}"]`).val();
-
-    store.setItem(hr.text, value);
-  });
-
-  grid.append(time);
-  grid.append(textArea);
-  grid.append(saveButton);
-
-  container.append(grid);
+  var saveText = function() {
+    var textAreaId = this.id.split("-")[0];
+    var textArea = document.getElementById(textAreaId);
+    localStorage.setItem(textAreaId, textArea.value);
+    console.log(this);
+  }
+  saveBtn.forEach(element => {
+    element.addEventListener("click", saveText);
+  })
 });
+
+
+
